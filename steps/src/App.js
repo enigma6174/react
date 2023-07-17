d77370222b856a457c63c4de5861ea1c0b00cbb0
enigma6174 <enigma6174@outlook.com>
2023-07-17 12:45:04 +0530

    Modified the step form to have reusable button and message components that take in a child prop

--- steps/src/App.js
+++ steps/src/App.js
@@ -6,8 +6,8 @@ const App = () => {
   return (
     <>
       <StepContainer />
-      <StepContainer />
-      <StepContainer />
+      {/* <StepContainer />
+      <StepContainer /> */}
     </>
   );
 };
@@ -36,27 +36,39 @@ const StepContainer = () => {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <Message step={step}>{messages[step - 1]}</Message>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈</span> Previous
+            </Button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next <span>👉</span>
+            </Button>
           </div>
-        </div>
+        </div>g
       )}
     </div>
+  );
+};
+
+const Message = ({ step, children }) => {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+};
+
+const Button = ({ textColor, bgColor, onClick, children }) => {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 };
 
